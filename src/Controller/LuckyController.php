a85b4e3f3d1dce271d739e492765eae15ad0c443
hoatq <hoatq@namviet-corp.vn>
2017-12-08 18:12:31 +0700

    edit

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,37 +2,56 @@
 
 namespace App\Controller;
 
-//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 //use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use GuzzleHttp\Client;
 use Psr\Log\LoggerInterface;
+
 //use GuzzleHttp\Psr7\Request;
 class LuckyController extends Controller
 {
-    const api = "fee/getAll";
     const ENDPOINT_NAME = 'WEB_BACKEND';
     const ENDPOINT_USER = 'admin';
     const ENDPOINT_PASS = 'namviet@2017';
     const ENDPOINT_KEY = 'ssd23ey8fdkjf@63293';
     const LANGCODE = 'vi'; //'en'
 
-    public function api()
+    protected function getDataApi($api, $data = null)
     {
-        $data = json_encode(array(
-            'id' => 1,
-        ));
-        $params = array(
-            'serviceEndpointName' => self::ENDPOINT_NAME,
-            'data' => $data,
-            'checksum' => md5(self::ENDPOINT_NAME . $data . self::ENDPOINT_KEY),
-            'langCode' => self::LANGCODE,
-        );
+        if ($data && !empty($data)) {
+            $params = array(
+                'serviceEndpointName' => self::ENDPOINT_NAME,
+                'data'                => $data,
+                'checksum'            => md5(self::ENDPOINT_NAME . $data . self::ENDPOINT_KEY),
+                'langCode'            => self::LANGCODE,
+            );
+        } else {
+            $params = array(
+                'serviceEndpointName' => self::ENDPOINT_NAME,
+                'checksum'            => md5(self::ENDPOINT_NAME . self::ENDPOINT_KEY),
+                'langCode'            => self::LANGCODE,
+            );
+        }
         $client   = new Client(['base_uri' => URL_API]);
-        $response = $client->request('POST', self::api,['json' => $params]);
-        return new Response($response->getBody());
+        $response = $client->request('POST', $api, ['json' => $params]);
+        return $response->getBody();
+    }
+    public function getFeeAll()
+    {
+        $api    = 'fee/getAll';
+        $result = $this->getDataApi($api);
+        return new Response($result);
+    }
+    public function getFeeId(Request $request)
+    {
+        $api    = 'fee/getAll';
+        $result = $this->getDataApi($api);
+        return new Response($request->all());
     }
 
 
 }
+
+?>
