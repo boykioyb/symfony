87209f5d4c2cb7867b1792c03898cc99121e5932
hoatq <hoatq@namviet-corp.vn>
2017-12-08 16:42:12 +0700

    add monolog

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -7,13 +7,23 @@ use Symfony\Component\HttpFoundation\Response;
 //use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use GuzzleHttp\Client;
+use Psr\Log\LoggerInterface;
 //use GuzzleHttp\Psr7\Request;
 class LuckyController extends Controller
 {
     const api = "fee/getAll";
 
-    public function api(){
-        $client = new Client(['base_uri'=> URL_API]);
+    public function api(LoggerInterface $logger)
+    {
+        $logger->info('I just got the logger');
+        $logger->error('An error occurred');
+
+        $logger->critical('I left the oven on!', array(
+            // include extra "context" info in your logs
+            'cause' => 'in_hurry',
+        ));
+
+        $client   = new Client(['base_uri' => URL_API]);
         $response = $client->request('GET', self::api);
         return new Response($response->getBody());
     }
